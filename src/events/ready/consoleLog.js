d8f1281ab6e866b621ebda9d602672a9b65eeb39
Smoky1111 <mattsearle1@gmail.com>
2023-05-03 06:49:24 +1000

    Update - Working

--- src/events/ready/consoleLog.js
+++ src/events/ready/consoleLog.js
@@ -1,5 +1,5 @@
 module.exports = (client) => {
     console.log(`+==========+ ${client.user.tag} is online - Running on local machine`),
     console.log('+==========+ Current Functions are very limited'),
-    console.log('+==========+ 2023/04/03');
+    console.log('+==========+ Updated 2023/04/03');
 };
