38b0ca3ff5ee8579cd0c18b0f4536fd0c9517bc2
Smoky1111 <mattsearle1@gmail.com>
2023-05-03 06:46:36 +1000

    Update - working

--- src/utils/getLocalCommands.js
+++ src/utils/getLocalCommands.js
@@ -4,6 +4,13 @@ const getAllFiles = require('./getAllFiles');
 module.exports = (exceptions) => {
     let localCommands = [];
 
-    
+    const commandCategories = getAllFiles(
+        path.join(__dirname, '../commands'),
+        true
+    );
 
-}
+    console.log(commandCategories);
+
+    return localCommands;
+
+};
