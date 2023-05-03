5976204465ac092a01f12c781a1e304c2d90ff18
Smoky1111 <mattsearle1@gmail.com>
2023-05-03 14:13:44 +1000

    Updated console.log

--- src/events/ready/consoleLog.js
+++ src/events/ready/consoleLog.js
@@ -1,5 +1,5 @@
 module.exports = (client) => {
-    console.log(`+==========+ ${client.user.tag} is online - Running on local machine`),
+    console.log(`+==========+ ${client.user.tag} is online - Running on RDT`),
     console.log('+==========+ Current Functions are very limited'),
     console.log('+==========+ Updated 2023/04/03');
 };
